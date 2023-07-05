7ff7978dffcdcc511f0bfab4a085cf423c1f4ef0
KartikeyaSharma <sharmakartikeya27@gmail.com>
2023-07-06 03:34:04 +0530

    Changed workers to 1 due to deployment limitations

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -3,7 +3,7 @@ import os
 from distutils.util import strtobool
 
 bind = os.getenv('WEB_BIND', '0.0.0.0:8000')
-workers = int(os.getenv('WEB_CONCURRENCY', multiprocessing.cpu_count() * 2))
+workers = int(os.getenv('WEB_CONCURRENCY', 1))
 threads = int(os.getenv('PYTHON_MAX_THREADS', 1))
 worker_class = os.getenv('WORKER_CLASS', 'uvicorn.workers.UvicornWorker')
 reload = bool(strtobool(os.getenv('WEB_RELOAD', 'false')))
